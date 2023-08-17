a1577ef21069024b5e33c244ff82ed45c57b6216
Kamili Empire <josephaoga@hotmail.com>
2023-08-17 12:59:57 +0300

    added paypal.me links for collecting donations

--- assets/js/main.js
+++ assets/js/main.js
@@ -118,9 +118,7 @@
     }
   }, true)
 
-  /**
-   * Scroll with ofset on page load with hash links in the url
-   */
+  /* scroll with ofset on page load with hash links in the url */
   window.addEventListener('load', () => {
     if (window.location.hash) {
       if (select(window.location.hash)) {
